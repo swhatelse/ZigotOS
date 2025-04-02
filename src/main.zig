e4d238a8916573ba1dc86f38ebe051b3c5daa158
Steven Masnada <steven.masnada@ubikasec.com>
2025-04-02 21:06:06 +0200

    feat: basic vga console
    working vga

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,5 @@
+const term = @import("terminal.zig");
+
 // Boot headers values
 const ALIGN = 1 << 0;
 const MEMINFO = 1 << 1;
@@ -14,6 +16,21 @@ const MultiBootHeader = packed struct {
 
 export var multibootheader: MultiBootHeader align(4) linksection(".multiboot") = .{};
 
+var stack: [16 * 1024]u8 align(16) linksection(".bss") = undefined;
+
 export fn _start() callconv(.Naked) noreturn {
+    asm volatile (
+        \\ movl %[stack_top], %%esp
+        \\ movl %%esp, %%ebp
+        \\ call %[kmain:P]
+        :
+        : [stack_top] "i" (@as([*]align(16) u8, @ptrCast(&stack)) + @sizeOf(@TypeOf(stack))),
+          [kmain] "X" (&kmain),
+    );
+}
+
+fn kmain() callconv(.C) void {
+    term.init();
+    term.puts("Hello Zig Kernel!");
     while (true) {}
 }
